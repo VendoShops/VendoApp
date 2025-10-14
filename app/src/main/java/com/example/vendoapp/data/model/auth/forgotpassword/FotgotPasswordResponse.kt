d26925e7cbefdb141a9987a495005bbc65c49b16
User <memmedlinihad103@gmail.com>
2025-10-14 19:40:32 +0300

    feature: updated login register

--- app/src/main/java/com/example/vendoapp/data/model/auth/forgotpassword/FotgotPasswordResponse.kt
+++
@@ -1,9 +0,0 @@
-package com.example.vendoapp.data.model.auth.forgotpassword
-
-data class ForgotPasswordResponse(
-    val success: Boolean,
-    val message: String,
-    val errorCode: Int? = null,
-    val path: String? = null,
-    val timestamp: String? = null
-)
