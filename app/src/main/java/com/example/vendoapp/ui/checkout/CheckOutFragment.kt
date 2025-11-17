bb3ba580a31344223f686a1605f178346d3443fe
Nihat <154012759+nihatmahammadli@users.noreply.github.com>
2025-11-18 02:07:09 +0400

    added checkout ui

---
+++ app/src/main/java/com/example/vendoapp/ui/checkout/CheckOutFragment.kt
@@ -0,0 +1,13 @@
+package com.example.vendoapp.ui.checkout
+
+import com.example.vendoapp.databinding.FragmentCheckOutBinding
+import com.example.vendoapp.ui.base.BaseFragment
+
+class CheckOutFragment : BaseFragment<FragmentCheckOutBinding>(
+    FragmentCheckOutBinding::inflate
+) {
+    override fun onViewCreateFinish() {
+        TODO("Not yet implemented")
+    }
+
+}
